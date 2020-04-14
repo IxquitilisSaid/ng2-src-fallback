5b60ade61823669fbf77aa1e0c21dbaa7112e8ba
Jaime Oliveira <Jaime.Oliveira@softgarden.de>
2020-04-14 10:55:05 +0200

    inno dud

--- karma.conf.js
+++ karma.conf.js
@@ -9,24 +9,34 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ng2-src-fallback'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeCustom: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--cap-add=SYS_ADMIN'] //evil hack for bamboo
+      },
+      ChromeDebug: {
+        base: 'Chrome',
+        flags: ['--remote-debugging-port=9229']
+      }
+    },
     singleRun: false,
-    restartOnFileChange: true
+    junitReporter: {
+        outputDir: 'target', // results will be saved as $outputDir/$browserName.xml
+        outputFile: 'angular-report.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+        suite: '', // suite will become the package name attribute in xml testsuite element
+        useBrowserName: false, // add browser name to report and classes names
+    }
   });
 };
